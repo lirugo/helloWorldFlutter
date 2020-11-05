87a3380de8b0afb1db2cce96804eacb72d2a15b0
roostik21@gmail.com <roostik21@gmail.com>
2020-11-05 15:16:14 +0200

    Drawer added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:ui';
 
 import 'package:built_collection/built_collection.dart';
 import 'package:flutter/material.dart';
@@ -69,6 +70,61 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
       ),
       body: Center(
         child: widget,
+      ),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            Container(
+              color: Colors.red,
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(0, 40, 0, 18),
+                child: Row(
+                  children: [
+                    new Padding(padding: new EdgeInsets.only(left: 10.0, right: 20.0),
+                        child: new CircleAvatar(radius: 35.0, backgroundImage: NetworkImage('https://img1.grunge.com/img/uploads/2018/11/scientific-effects-happen-man-grows-beard.jpg'),)
+                    ),
+                    Text('Иванов Иван', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20))
+                  ],
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.access_alarm),
+              title: Text('Напоминания'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.bar_chart),
+              title: Text('Отчеты'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.group),
+              title: Text('Пользователи'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
