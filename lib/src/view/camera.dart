464c0ab6b6841a6565167acd2abf4e79518f810c
roostik21@gmail.com <roostik21@gmail.com>
2020-11-05 17:45:41 +0200

    Camera float action button added

--- lib/src/view/camera.dart
+++ lib/src/view/camera.dart
@@ -1,4 +1,5 @@
 import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class Camera extends StatefulWidget {
@@ -11,13 +12,18 @@ class Camera extends StatefulWidget {
 }
 
 class _CameraState extends State<Camera> {
-  CameraController controller;
+  CameraController _controller;
+  Future<void> _initializeControllerFuture;
+
   @override
   void initState() {
     super.initState();
-    if(widget.cameras.length > 0) {
-      controller = CameraController(widget.cameras[0], ResolutionPreset.medium);
-      controller.initialize().then((_) {
+    if (widget.cameras.length > 0) {
+      _controller =
+          CameraController(widget.cameras.first, ResolutionPreset.medium);
+
+      // Next, initialize the controller. This returns a Future.
+      _initializeControllerFuture = _controller.initialize().then((_) {
         if (!mounted) {
           return;
         }
@@ -28,19 +34,33 @@ class _CameraState extends State<Camera> {
 
   @override
   void dispose() {
-    controller?.dispose();
+    _controller?.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    if (controller == null || !controller.value.isInitialized) {
+    if (_controller == null || !_controller.value.isInitialized) {
       return Container();
     }
 
-    return AspectRatio(
-        aspectRatio:
-        controller.value.aspectRatio,
-        child: CameraPreview(controller));
+    return Scaffold(
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // If the Future is complete, display the preview.
+            return CameraPreview(_controller);
+          } else {
+            // Otherwise, display a loading indicator.
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.camera_alt),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
   }
 }
